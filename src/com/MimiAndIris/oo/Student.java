4b01aba3a6bf6fc4a0caceb9214c34a2e78dedd2
IrisAndMimi <my1ris227@outlook.com>
2017-12-05 16:33:37 +0800

    com.MimiAndIris.oo:物件導向,建構子,屬性&方法

---
+++ src/com/MimiAndIris/oo/Student.java
@@ -0,0 +1,22 @@
+package com.MimiAndIris.oo;
+
+public class Student {
+	int english;
+	int math;
+	int chinese;
+	String name;
+	
+	public Student(String n,int e,int m,int c){
+		name = n;
+		english = e;
+		math = m;
+		chinese = c;
+	}
+		public void print(){
+		System.out.println(english+ "\t" +math +"\t"+chinese);
+	}
+	public int getAverage(){
+		return (english+math+chinese)/3;
+	}
+	
+}
