b5dff8e9e0fd1f6dde98f0e78bce75fa8b2a2d72
IrisAndMimi <my1ris227@outlook.com>
2017-12-05 14:02:15 +0800

    BubbleSort

--- src/com/MimiAndIris/BubleSort.java
+++ src/com/MimiAndIris/BubleSort.java
@@ -3,8 +3,21 @@ package com.MimiAndIris;
 public class BubleSort {
 
 	public static void main(String[] args) {
-		int n[]={3, 2, 1};
-
+		int n[]={55, 6, 31, 77, 13};
+////			  0	 1   2   3   4
+		for (int i=0; i<n.length-1; i++){
+			for (int j=i+1; j<n.length; j++){
+//				System.out.println(i +","+j);
+				if (n[i]>n[j]){
+					int tmp = n[i];
+					n[i] = n[j];
+					n[j] = tmp;
+				}
+			}
+		}
+		for (int num: n){
+			System.out.print(num + " ");
+		}
 	}
 
 }
