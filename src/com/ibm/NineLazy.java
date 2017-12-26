8fd33e42ec74523a3e82bdbe686542d77e428144
IrisAndMimi <my1ris227@outlook.com>
2017-12-26 14:21:13 +0800

    NineLazy更新:不會咳嗽和拉肚子了

--- src/com/ibm/NineLazy.java
+++ src/com/ibm/NineLazy.java
@@ -1,21 +1,22 @@
 package com.ibm;
 
 public class NineLazy {
-	public void nine(){
-		for (int n=2; n<=9 ;n++){
-			if (a*n/10 ==0){
-				System.out.print(n+" x "+a+" =  "+a*n);
-			}else{
-				System.out.print(n+" x "+a+" = "+a*n);
-			}
-			System.out.print("\t");
-		}
-	}
+//	public void nine(){
+//		for (int n=2; n<=9 ;n++){
+//			if (a*n/10 ==0){
+//				System.out.print(n+" x "+a+" =  "+a*n);
+//			}else{
+//				System.out.print(n+" x "+a+" = "+a*n);
+//			}
+//			System.out.print("\t");
+//		}
+//	}
 
 	public static void main(String[] args) {
 		int a=0;
-		while(a<3){
-			a = a+1;
+		while(a<9){
+			if  ((a<3)||(a>7)){
+				a = a+1;
 				for (int n=2; n<=9 ;n++){
 					if (a*n/10 ==0){
 						System.out.print(n+" x "+a+" =  "+a*n);
@@ -25,32 +26,12 @@ public class NineLazy {
 					System.out.print("\t");
 				}
 				System.out.println();
+			}else{
+				a = a+1;
+				System.out.println("...\t\t...\t\t...\t\t...\t\t...\t\t...\t\t...\t\t...");
 			}
-		while(a<8){
-			a = a+1;
-			System.out.print("\t...");
-			System.out.print("\t\t...");
-			System.out.print("\t\t...");
-			System.out.print("\t\t...");
-			System.out.print("\t\t...");
-			System.out.print("\t\t...");
-			System.out.print("\t\t...");
-			System.out.println("\t\t...");
 		}
-		
-		while(a==8){
-			a = a+1;
-			for (int n=2; n<=9 ;n++){
-				if (a*n/10 ==0){
-					System.out.print(n+" x "+a+" =  "+a*n);
-				}else{
-					System.out.print(n+" x "+a+" = "+a*n);
-				}
-				System.out.print("\t");
-			}
-			System.out.println();
-		}
-//		}
-		}
+
+	}
 
 }
