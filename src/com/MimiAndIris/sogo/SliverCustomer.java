43a67c45593008d0f16e22e92b85e1d9ecc15968
IrisAndMimi <my1ris227@outlook.com>
2018-01-09 14:35:19 +0800

    ArrayList

--- src/com/MimiAndIris/sogo/SliverCustomer.java
+++ src/com/MimiAndIris/sogo/SliverCustomer.java
@@ -3,7 +3,12 @@ package com.MimiAndIris.sogo;
 public class SliverCustomer extends Customer {
 	public SliverCustomer(int amount){
 		super(amount);
-		
+		super.discount = 0.9f;
 	}
-
+//	@Override
+//	public void print(){
+//		int total = (int)(amount*discount);
+//		System.out.println(total);
+//
+//	}
 }
