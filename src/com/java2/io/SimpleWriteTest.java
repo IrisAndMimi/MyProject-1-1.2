230d1ea47048e2489264b408e6a8daadec3dcbdd
IrisAndmimi <my1ris227@outlook.com>
2018-04-17 16:10:16 +0800

    @@

--- src/com/java2/io/SimpleWriteTest.java
+++ src/com/java2/io/SimpleWriteTest.java
@@ -3,11 +3,11 @@ package com.java2.io;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
-public class SimpleTester {
+public class SimpleWriteTest {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		PrintStream out = new PrintStream("file.txt");
-		out.print("Mimi/t01/t500");
+		PrintStream out = new PrintStream("data.txt");
+		out.print("Mimi/t1/t500");
 		out.flush();
 		out.close();
 	}
