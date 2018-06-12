be5d5c6fd917aa977fd0f6bdfa7fabc97b9cd16e
IrisAndMimi <my1ris227@outlook.com>
2018-06-12 16:28:51 +0800

    bingo-generate

--- src/com/java2/oo/BingoMain.java
+++ src/com/java2/oo/BingoMain.java
@@ -2,6 +2,7 @@ package com.java2.oo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Random;
 
 public class BingoMain {
@@ -16,6 +17,26 @@ public class BingoMain {
 //			System.out.println(ary);
 			Random rd = new Random();
 			
+			int bgo[] = new int[]{};
+//			ArrayList<Integer> bgo = new ArrayList<Integer>();
+			for(int i=0; i<10;i++){
+				bgo.add(i);
+			}
+			   for(int i=0; i < bgo.length; i ++){
+			     int index = rd.nextInt(10);
+			     int tmp = bgo(index);
+			     bgo[index] = bgo[i];
+			     bgo[i] = tmp;
+			   }
+			   for(int i=0; i < bgo.length; i ++){
+				   if (i+1==5){
+					   System.out.println(bgo[i]);
+				   }else{
+					   System.out.print(bgo[i]+" ");
+				   }
+			   }
+			
+		
 			String[] ar = br.readLine().split(","); 
 			
 //			for (int i = 0; i < ar.length; i++) { 
